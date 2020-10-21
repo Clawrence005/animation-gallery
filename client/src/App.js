a6ad0672a1918fa4cce0f551de250beb56001e47
colin <clawrence005@yahoo.com>
2020-10-21 14:09:31 -0500

    styling blogDetail, need to fully abstratct later

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 import Nav from './components/Nav'
+import './components/BlogPageDetail/style.css';
 
 export const HomePage = () => {
   return (
@@ -38,37 +39,46 @@ const PageNotFound = () => {
 
 const BlogPageDetail = ({ match }) => {
 
-  const pageFilter = blogData.filter(x => x === match.params.id)
-  console.log(pageFilter)
+  // const pageFilter = blogData.filter(x => x === match.params.id)
+  // console.log(pageFilter)
   return (
-    <div >
+    <div className='blog-detail-wrapper'
+    >
       {blogData.filter(person => person.blogTitle === match.params.id).map(filteredPerson => (
-        <li key={filteredPerson.blogTitle}>
-          <h2>{filteredPerson.blogTitle}</h2>
+        <div key={filteredPerson.blogTitle}>
+          <h2 className='blog-title'>{filteredPerson.blogTitle}</h2>
+          <img src={filteredPerson.blogImage} alt={filteredPerson.blogTitle} className="blog-image" />
           <p>{filteredPerson.blogDescrip}</p>
-          <p>{filteredPerson.blogBody}</p>
-        </li>
+          <div className="blog-body">
+            <p>{filteredPerson.blogBody}</p>
+          </div>
+        </div>
+
       ))}
 
     </div>
   )
 }
 const blogData = [{
-  blogTitle: "blogTitle1",
-  blogDescrip: "blog decrip 1",
-  blogBody: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  blogTitle: "Dog",
+  blogDescrip: "A dog is a 4 legged creature.",
+  blogImage: "/assets/cat-image.jpg",
+  blogBody: " Lorem Dog ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 }, {
-  blogTitle: "blogTitle2",
-  blogDescrip: "blog decrip2",
+  blogTitle: "Ferret",
+  blogDescrip: "A ferret is a cat snake.",
+  blogImage: "/assets/cat-image.jpg",
+  blogBody: "Ferret lorem m1dsfc sdfsdfsd fsdf sdfsdfs df sdf sdfs fsdf ds fsdf sdf ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidat"
+}, {
+  blogTitle: "Plant",
+  blogDescrip: "A plant is now a pet too!",
+  blogImage: "/assets/cat-image.jpg",
   blogBody: "lore m1dsfc sdfsdfsd fsdf sdfsdfs df sdf sdfs fsdf ds fsdf sdf"
 }, {
-  blogTitle: "blogTitle3",
-  blogDescrip: "blog decrip3",
-  blogBody: "lore m1dsfc sdfsdfsd fsdf sdfsdfs df sdf sdfs fsdf ds fsdf sdf"
-}, {
-  blogTitle: "blogTitle4",
-  blogDescrip: "blog decrip4",
-  blogBody: "lore m1dsfc sdfsdfsd fsdf sdfsdfs df sdf sdfs fsdf ds fsdf sdf"
+  blogTitle: "Cat",
+  blogDescrip: "a cat has four legs too and makes weird sounds",
+  blogImage: "/assets/cat-image.jpg",
+  blogBody: "lore m1dsfc sdfsdfsd fsdf sdfsdfs df sdf sdfs fsdf ds fsdf sdfnostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur s"
 }
 ];
 const BlogPage = () => {
@@ -90,16 +100,19 @@ function App() {
   return (
     <Router>
       <Nav />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/cats' component={CatPage} />
-        <Route exact path='/cats/:id' component={CatDetail} />
-        <Route exact path='/blog' component={BlogPage} />
-        <Route exact path='/blog/:id' component={BlogPageDetail} />
-        <Route component={PageNotFound} />
+      < main>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/cats' component={CatPage} />
+          <Route exact path='/cats/:id' component={CatDetail} />
+          <Route exact path='/blog' component={BlogPage} />
+          <Route exact path='/blog/:id' component={BlogPageDetail} />
+          <Route component={PageNotFound} />
 
-      </Switch>
+        </Switch>
+      </ main>
     </Router>
+
   );
 }
 
---
+++ client/src/components/BlogPageDetail/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './style.css';
+
+
+// const BlogPageDetail = (props) => {
+//   return (
+//     <div>
+//       {console.log(props)}
+//       {/* {props.blogData.map((item) => <div>{item.blogTitle}
+//       </div>)} */}
+//     </div>
+//   )
+// }
+
+// const BlogPageDetail = ({ props }) => {
+
+//   const pageFilter = blogData.filter(x => x === match.params.id)
+//   console.log(pageFilter)
+//   return (
+//     <div className="blog-detail-wrapper">
+//       {props.blogData.filter(person => person.blogTitle === match.params.id).map(filteredPerson => (
+//         <div key={filteredPerson.blogTitle}>
+//           <h2 className='blog-title'>{filteredPerson.blogTitle}</h2>
+//           <img src={filteredPerson.blogImage} alt={filteredPerson.blogTitle} className="blog-image" />
+
+//           <p className="blog-descrip">{filteredPerson.blogDescrip}</p>
+//           <div className="blog-body">
+//             <p>{filteredPerson.blogBody}</p>
+//           </div>
+//         </div>
+//       ))}
+
+//     </div>
+//   )
+// }
+
+export default BlogPageDetail;
